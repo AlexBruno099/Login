3252010c2ac80267a075ef899fa1af1bf74b8fd8
AlexBruno099 <alexbrunorr099@gmail.com>
2024-01-20 13:27:35 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+
+class Program
+{
+    static void Main()
+    {
+        string Usuario = "Alex";
+        string Senha = "Dev";
+
+        Console.WriteLine("Por favor, realize o login.");
+
+        Console.WriteLine("Usuário:");
+        string usuarioDigitado = Console.ReadLine();
+
+        Console.WriteLine("Senha:");
+        string senhaDigitada = Console.ReadLine();
+
+        if (ValidarLogin(usuarioDigitado, senhaDigitada, Usuario, Senha))
+        {
+            Console.WriteLine("Login bem-sucedido!");
+        }
+        else
+        {
+            Console.WriteLine("Usuário ou senha incorretos. Tente novamente.");
+        }
+
+        Console.ReadLine();
+    }
+
+    static bool ValidarLogin(string usuarioDigitado, string senhaDigitada, string Usuario, string Senha)
+    {
+        return usuarioDigitado == Usuario && senhaDigitada == Senha;
+    }
+    
+}
